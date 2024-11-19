4aac2c72607b3e0aa03431d969ea20bcbf2f1914
lahiruileperumage <lahiru212001@gmail.com>
2024-11-19 23:35:09 +0530

    after submit user application success alert and clear lines

--- src/application/rating.js
+++ src/application/rating.js
@@ -3,13 +3,14 @@ import JobApplication from "../persistance/entity/jobsApplications.js";
 
 const client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 export default async function generateRating(jobApplicationId) {
+    console.log("Calling fine tuning model")
     const jobApplication = await JobApplication.findById(jobApplicationId).populate("job");
     const content = `Role:${jobApplication?.job.title}, User Description : ${jobApplication?.answers.join(". ")}`
     
     const completion = await client.chat.completions.create(
         {
             messages:[{role:"user",content}],
-            model: "ft:gpt-3.5-turbo-0125:stemlink:cleverlyal:ARfciwc8"
+            model: "ft:gpt-3.5-turbo-0125:stemlink:fullstacktutorial:AVERxq2j"
         }
     );
     const response = JSON.parse(completion.choices[0].message.content);
