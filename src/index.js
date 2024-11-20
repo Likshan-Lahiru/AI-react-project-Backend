9077aa47989dc258dd32a660c8a2be30c2128034
lahiruileperumage <lahiru212001@gmail.com>
2024-11-20 12:55:35 +0530

    cors update

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import globalErrorHandlingMiddleware from "./api/middleware/global-error-handlin
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "*" }));
 
 connectionDB();
 
